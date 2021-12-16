2e392e46b5f065bafa4859030583f275f2b04515
Ivan Mengual <ivanmengualruiz@gmail.com>
2021-12-16 13:16:22 +0100

    n vhv

---
+++ index.js
@@ -0,0 +1,10 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+
+const calculadora = new Calculadora();
+
+console.log(calculadora.sumar(2,3))
+console.log(calculadora.sumar(2,8))
